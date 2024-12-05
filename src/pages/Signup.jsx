53b80abab0bdb533f438ab837b5e039a068a57ab
Kang Dabin <kchabin@swu.ac.kr>
2024-12-05 21:12:41 +0900

    Signup.jsx 고정 ip 적용

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -89,7 +89,7 @@ const Signup = () => {
               email: "",
               userType: "personal",
             });
-            window.location.href = "http://localhost:5173/login";
+            window.location.href = "http://15.165.54.60:8000/login";
           }
         } catch (err) {
           if (err.response && err.response.data) {
